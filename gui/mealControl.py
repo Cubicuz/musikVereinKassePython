2b24265dd241d4be38e7b189efc6f20d08fff002
Cubicuz <contactme@through.github>
2022-10-04 12:35:52 +0200

    validation is trigered on manual input but the native changedhandler not wich is unexpected to me

--- gui/mealControl.py
+++ gui/mealControl.py
@@ -8,24 +8,54 @@ sys.path.append('../musikVereinKassePython')
 
 from logic.dining.meal import Meal
 
+# Validating function
+def numberValidator(user_input):
+    # check if the input is numeric
+    if  user_input.isdigit():
+        # Fetching minimum and maximum value of the spinbox
+        minval = int(0)
+        maxval = int(100)
+ 
+        # check if the number is within the range
+        if int(user_input) not in range(minval, maxval):
+            print ("Out of range")
+            return False
+ 
+        # Printing the user input to the console
+        print(user_input)
+        return True
+ 
+    # if input is blank string
+    elif user_input == "":
+        print(user_input)
+        return True
+ 
+    # return false is input is not numeric
+    else:
+        print("Not numeric ", user_input)
+        return False
+
 class MealControl:
     def __init__(self, root, meal):
         self._root = root
         self._frame = ttk.Frame(root, padding=10)
         self._meal = meal
-
+        self._amount = tk.IntVar(value=0)
         def valueChanged():
-            print(self._spinbox.get()) 
+            print(self._amount.get()) 
 
         self._spinbox = ttk.Spinbox(
             self._frame,
             from_=0, increment=1, to=100, 
-            textvariable=tk.StringVar(value=0), 
+            textvariable=self._amount, 
             command=valueChanged)
         self._spinbox.grid(column=0, row=0)
         self._frame.pack()
         s = ttk.Style(self._frame)
         s.configure('TFrame', background="red")
+
+        validator = self._frame.register(numberValidator)
+        self._spinbox.config(validate="key", validatecommand=(validator, '%P'))
 
 
 
